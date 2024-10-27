95eaa2c3c5fb7e5adf937944fe5c7df5f14e2de8
Harsh <86718309+HarsharajIrkar@users.noreply.github.com>
2024-10-27 23:16:45 +0530

    violet: prepare vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/rdx420/vendor_xiaomi_violet.git -b eleven vendor/xiaomi/violet
 git clone https://github.com/karthik558/MsM-4.14-RyZeN.git -b R kernel/xiaomi/violet
-git clone https://gitlab.com/dotos-devices/vendor_ANXCamera.git -b dot11 vendor/ANXCamera
+git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b eleven vendor/ANXCamera
